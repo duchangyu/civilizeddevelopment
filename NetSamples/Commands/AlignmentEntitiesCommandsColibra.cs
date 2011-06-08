a714654296fae4ceb695935da6c3d490f923b0ac
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2011-06-08 15:55:22 -0400

    Added implementation of sample alignment commands using Colibra.

---
+++ NetSamples/Commands/AlignmentEntitiesCommandsColibra.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.Land.DatabaseServices;
+
+using Colibra;
+
+[assembly: CommandClass(typeof(Autodesk.CivilizedDevelopment.AlignmentEntitiesCommandsColibra))]
+
+namespace Autodesk.CivilizedDevelopment
+{
+    public class AlignmentEntitiesCommandsColibra
+    {
+        [CommandMethod("CDS_WriteAlignmentEntitiesById")]
+        public void CDS_WriteAlignmentEntitiesById()
+        {
+            doWriteAlignmentEntities(new ByEntityIdEnumerationPolicy());
+        }
+
+        [CommandMethod("CDS_WriteAlignmentEntitiesBySequence")]
+        public void CDS_WriteAlignmentEntitiesBySequence()
+        {
+            doWriteAlignmentEntities(new BySequenceEnumerationPolicy());
+        }
+
+        private void doWriteAlignmentEntities(
+            IAlignmentEntityEnumerationPolicy enumerationPolicy)
+        {
+            Document current = DocumentManager.ActiveDocument;
+            using (Transaction tr = current.StartTransaction())
+            {
+                // First we select the alignment to write.
+                //
+                SingleObjectSelector<Alignment> selector =
+                new SingleObjectSelector<Alignment>();
+                if (!selector.Select(current))
+                {
+                    _output.WriteMessage("\nNo Alignment was selected.");
+                    return;
+                }
+
+                // We create the processor and assign the policy.
+                //
+                AlignmentEntityProcessor processor =
+                    new AlignmentEntityProcessor(selector.SelectedId);
+                processor.EnumerationPolicy = enumerationPolicy;
+                
+                // We create the writer and write the information to it.
+                //
+                DocumentOutputAlignmentEntityInfoWriter writer =
+                    new DocumentOutputAlignmentEntityInfoWriter(current);
+                processor.WriteInfo(writer);
+            }
+        }
+
+        private Editor _output
+        {
+            get
+            {
+                return Autodesk.AutoCAD.ApplicationServices.Application
+                    .DocumentManager.MdiActiveDocument.Editor;
+            }
+        }
+    }
+}
