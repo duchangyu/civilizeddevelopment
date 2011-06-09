e9a76cc6412ec1bd78f3fb834ffd468aca62066d
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2011-06-08 22:33:49 -0400

    Added pack.cmd which creates a ZIP file for posting.
    I created pack.cmd to help me create the ZIP files ready for posting. The ZIP file is created from the list of file in filelist.txt.
    
    To pack the files just type at the command line:
    
    pack buildnum [subnum]

---
+++ pack.cmd
@@ -0,0 +1,33 @@
+@echo off
+
+:: Project variables.
+::
+set project=Colibra
+set vmajor=1
+set vminor=0
+if (%1)==() (
+  goto error_no_build_number
+) else (
+  set vbuild=%1
+)
+if (%2)==() (
+  set vsub=0
+) else (
+  set vsub=%2
+)
+
+:: Process variables.
+::
+set zip_util="C:\Program Files\7-Zip\7z.exe"
+set file_list=filelist.txt
+
+%zip_util% a -r -tzip %project%_%vmajor%_%vminor%_%vbuild%_%vsub%.zip @%file_list%
+
+goto end
+
+:error_no_build_number
+  echo ERROR: No build number specified.
+  echo Usage: pack buildnum [subnum]
+
+:end
+  echo Done.
