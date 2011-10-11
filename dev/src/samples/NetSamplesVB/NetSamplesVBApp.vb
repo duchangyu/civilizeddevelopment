25288063de7be614d0d5211a4d3794b98a337ba3
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2011-10-11 15:09:18 -0400

    Fixing path to VB Samples project and updating references.

---
+++ dev/src/samples/NetSamplesVB/NetSamplesVBApp.vb
@@ -0,0 +1,19 @@
+﻿Imports Autodesk.AutoCAD.Runtime
+
+<Assembly: ExtensionApplication( _
+    GetType(Autodesk.CivilizedDevelopment.NetSamplesApp))> 
+
+Namespace Autodesk.CivilizedDevelopment
+    Public Class NetSamplesApp
+        Implements IExtensionApplication
+        Public Sub Initialize() _
+            Implements IExtensionApplication.Initialize
+
+        End Sub
+
+        Public Sub Terminate() _
+            Implements IExtensionApplication.Terminate
+
+        End Sub
+    End Class
+End Namespace
