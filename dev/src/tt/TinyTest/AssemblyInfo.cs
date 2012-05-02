3ffa5381041fe49239e931d2f023670dde43ba17
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2012-05-02 11:36:40 -0400

    Reversioning project to 2.0.2.1.

--- dev/src/tt/TinyTest/AssemblyInfo.cs
+++ dev/src/tt/TinyTest/AssemblyInfo.cs
@@ -65,7 +65,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
+[assembly: AssemblyVersion("2.0.2.1")]
+[assembly: AssemblyFileVersion("2.0.2.1")]
 
 [assembly: InternalsVisibleTo("TinyTestTesting")]
