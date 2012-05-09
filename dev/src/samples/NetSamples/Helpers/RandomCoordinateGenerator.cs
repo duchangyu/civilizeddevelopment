d254ab0e5511c5766a745acd2cccf39a96ccd712
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2012-05-09 09:21:34 -0400

    Implemented "CDS_GenerateRandomPoints" command.
    This command serves as demonstration of the basic functionality in the COGO Point object. The command demonstrates how to create COGO Points and display some of its properties.
    
    I added a 'RandomCoordinateGenerator' class to produce random Point3d objects per requests.

---
+++ dev/src/samples/NetSamples/Helpers/RandomCoordinateGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Autodesk.AutoCAD.Geometry;
+
+namespace Autodesk.CivilizedDevelopment
+{
+    public class RandomCoordinateGenerator
+    {
+        public RandomCoordinateGenerator()
+        {
+            MaxX = 1000.0;
+            MaxY = 1000.0;
+            MaxZ = 100.0;
+            initGenerator();
+        }
+
+        public RandomCoordinateGenerator(double maxx, double maxy, double maxz)
+        {
+            MaxX = maxx;
+            MaxY = maxy;
+            MaxZ = maxz;
+            initGenerator();
+        }
+
+        public double MaxX { get; set; }
+        public double MaxY { get; set; }
+        public double MaxZ { get; set; }
+
+        public Point3d GetCoordinate()
+        {
+            double x = m_Generator.NextDouble() * MaxX;
+            double y = m_Generator.NextDouble() * MaxY;
+            double z = m_Generator.NextDouble() * MaxZ;
+            return new Point3d(x, y, z);
+        }
+
+        public Point3dCollection GetCoordinates(int count)
+        {
+            Point3dCollection result = new Point3dCollection();
+            for (int i = 0; i < count; i++)
+            {
+                result.Add(GetCoordinate());
+            }
+
+            return result;
+        }
+
+
+        private void initGenerator()
+        {
+            int seed = (int)DateTime.Now.Ticks;
+            m_Generator = new Random(seed);
+        }
+
+        private Random m_Generator;
+    }
+}
---
+++ dev/src/samples/NetSamples/Samples/CogoPoints/CogoPointCommands.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.DatabaseServices;
+
+[assembly: CommandClass(
+    typeof(Autodesk.CivilizedDevelopment.CogoPointCommands))]
+
+namespace Autodesk.CivilizedDevelopment
+{
+    public class CogoPointCommands : SimpleDrawingCommand
+    {
+        [CommandMethod("CDS_CreateRandomPoints")]
+        public void CDS_CreateRandomPoints()
+        {
+            RandomCoordinateGenerator generator = 
+                new RandomCoordinateGenerator();
+            CogoPointCollection points = _civildoc.CogoPoints;
+
+            // From Point3d
+            Point3d coordinate = generator.GetCoordinate();
+            ObjectId pointId = points.Add(coordinate);
+            write("\nSingle Point from coordinate.");
+            display(pointId);
+
+            coordinate = generator.GetCoordinate();
+            pointId = points.Add(coordinate, "Sample description.");
+            write("\nSingle Point from coordinate and description.");
+            display(pointId);
+
+            coordinate = generator.GetCoordinate();
+            pointId = points.Add(coordinate, "Sample description",  
+                true, false);
+            write("\nSingle Point from coordinate with description, "
+                + "using description key, and not matching parameters.");
+            display(pointId);
+
+            // From Point3dCollection
+            Point3dCollection coordinates = generator.GetCoordinates(10);
+            ObjectIdCollection pointIds = points.Add(coordinates);
+            write("\nPoints from coordinate collection.");
+            display(pointIds);
+
+            coordinates = generator.GetCoordinates(5);
+            pointIds = points.Add(coordinates, "Group of 5");
+            write("\nPoints from coordinate collection with description.");
+            display(pointIds);
+
+            coordinates = generator.GetCoordinates(7);
+            pointIds = points.Add(coordinates, "Group of 7", true, true);
+            write("\nPoints from coordinate collection with description,"
+                + "using description key, and not matching parameters.");
+            display(pointIds);
+        }
+
+        private void display(ObjectId pointId)
+        {
+            using (Transaction tr = startTransaction())
+            {
+                CogoPoint point = pointId.GetObject(OpenMode.ForRead) 
+                    as CogoPoint;
+                displayPointInfo(point);
+            }
+        }
+
+        private void display(ObjectIdCollection pointIds)
+        {
+            using (Transaction tr = startTransaction())
+            {
+                foreach (ObjectId pointId in pointIds)
+                {
+                    CogoPoint point = pointId.GetObject(OpenMode.ForRead)
+                        as CogoPoint;
+                    displayPointInfo(point);
+                }
+            }
+        }
+
+        private void displayPointInfo(CogoPoint point)
+        {
+            write("\nPoint Number: " + point.PointNumber.ToString());
+            write("\n- Location: " + point.Location.ToString());
+            write("\n- - Northing: " + point.Northing.ToString());
+            write("\n- - Easting: " + point.Easting.ToString());
+            write("\n- - Elevation: " + point.Elevation.ToString());
+            write("\n- Description: " + point.FullDescription);
+            write("\n- Raw Description: " + point.RawDescription);
+        }
+    }
+}
