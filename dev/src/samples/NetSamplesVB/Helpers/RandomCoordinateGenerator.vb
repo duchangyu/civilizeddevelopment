029f8f9b2084bc14089ebde507b3af806d0b6cc1
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2012-05-09 09:26:56 -0400

    Translated the "CDS_CreateRandomPoints" command to VB.NET.

--- dev/src/samples/NetSamplesVB/Helpers/RandomCoordinateGenerator.vb
+++ dev/src/samples/NetSamplesVB/Helpers/RandomCoordinateGenerator.vb
@@ -1,1 +1,71 @@
-﻿
+﻿Imports Autodesk.AutoCAD.Geometry
+
+Namespace Autodesk.CivilizedDevelopment
+  Public Class RandomCoordinateGenerator
+    Public Sub New()
+      MaxX = 1000.0
+      MaxY = 1000.0
+      MaxZ = 100.0
+      initGenerator()
+    End Sub
+
+    Public Sub New(maxx__1 As Double, maxy__2 As Double, maxz__3 As Double)
+      MaxX = maxx__1
+      MaxY = maxy__2
+      MaxZ = maxz__3
+      initGenerator()
+    End Sub
+
+    Public Property MaxX() As Double
+      Get
+        Return m_MaxX
+      End Get
+      Set(value As Double)
+        m_MaxX = Value
+      End Set
+    End Property
+    Private m_MaxX As Double
+    Public Property MaxY() As Double
+      Get
+        Return m_MaxY
+      End Get
+      Set(value As Double)
+        m_MaxY = Value
+      End Set
+    End Property
+    Private m_MaxY As Double
+    Public Property MaxZ() As Double
+      Get
+        Return m_MaxZ
+      End Get
+      Set(value As Double)
+        m_MaxZ = Value
+      End Set
+    End Property
+    Private m_MaxZ As Double
+
+    Public Function GetCoordinate() As Point3d
+      Dim x As Double = m_Generator.NextDouble() * MaxX
+      Dim y As Double = m_Generator.NextDouble() * MaxY
+      Dim z As Double = m_Generator.NextDouble() * MaxZ
+      Return New Point3d(x, y, z)
+    End Function
+
+    Public Function GetCoordinates(count As Integer) As Point3dCollection
+      Dim result As New Point3dCollection()
+      For i As Integer = 0 To count - 1
+        result.Add(GetCoordinate())
+      Next
+
+      Return result
+    End Function
+
+
+    Private Sub initGenerator()
+      Dim seed As Integer = CInt(DateTime.Now.Ticks)
+      m_Generator = New Random(seed)
+    End Sub
+
+    Private m_Generator As Random
+  End Class
+End Namespace
--- dev/src/samples/NetSamplesVB/Samples/CogoPoints/CogoPointsCommands.vb
+++ dev/src/samples/NetSamplesVB/Samples/CogoPoints/CogoPointsCommands.vb
@@ -1,1 +1,82 @@
-﻿
+﻿Imports Autodesk.AutoCAD.DatabaseServices
+Imports Autodesk.AutoCAD.Geometry
+Imports Autodesk.AutoCAD.Runtime
+Imports Autodesk.Civil
+Imports Autodesk.Civil.DatabaseServices
+
+<Assembly: CommandClass(GetType(Autodesk.CivilizedDevelopment.CogoPointCommands))> 
+
+Namespace Autodesk.CivilizedDevelopment
+  Public Class CogoPointCommands
+    Inherits SimpleDrawingCommand
+    <CommandMethod("CDS_CreateRandomPoints")> _
+    Public Sub CDS_CreateRandomPoints()
+      Dim generator As New RandomCoordinateGenerator()
+      Dim points As CogoPointCollection = _civildoc.CogoPoints
+
+      ' From Point3d
+      Dim coordinate As Point3d = generator.GetCoordinate()
+      Dim pointId As ObjectId = points.Add(coordinate)
+      write(vbLf & "Single Point from coordinate.")
+      display(pointId)
+
+      coordinate = generator.GetCoordinate()
+      pointId = points.Add(coordinate, "Sample description.")
+      write(vbLf & "Single Point from coordinate and description.")
+      display(pointId)
+
+      coordinate = generator.GetCoordinate()
+      pointId = points.Add(coordinate, "Sample description", True, False)
+      write(vbLf & "Single Point from coordinate with description, " _
+            & "using description key, and not matching parameters.")
+      display(pointId)
+
+      ' From Point3dCollection
+      Dim coordinates As Point3dCollection = generator.GetCoordinates(10)
+      Dim pointIds As ObjectIdCollection = points.Add(coordinates)
+      write(vbLf & "Points from coordinate collection.")
+      display(pointIds)
+
+      coordinates = generator.GetCoordinates(5)
+      pointIds = points.Add(coordinates, "Group of 5")
+      write(vbLf & "Points from coordinate collection with description.")
+      display(pointIds)
+
+      coordinates = generator.GetCoordinates(7)
+      pointIds = points.Add(coordinates, "Group of 7", True, True)
+      write(vbLf & "Points from coordinate collection with description," _
+            & "using description key, and not matching parameters.")
+      display(pointIds)
+    End Sub
+
+    Private Sub display(pointId As ObjectId)
+      Using tr As Transaction = startTransaction()
+        Dim point As CogoPoint =
+          TryCast(pointId.GetObject(OpenMode.ForRead), CogoPoint)
+        displayPointInfo(point)
+      End Using
+    End Sub
+
+    Private Sub display(pointIds As ObjectIdCollection)
+      Using tr As Transaction = startTransaction()
+        For Each pointId As ObjectId In pointIds
+          Dim point As CogoPoint =
+            TryCast(pointId.GetObject(OpenMode.ForRead), CogoPoint)
+          displayPointInfo(point)
+        Next
+      End Using
+    End Sub
+
+    Private Sub displayPointInfo(point As CogoPoint)
+      write(vbLf & "Point Number: " & point.PointNumber.ToString())
+      write(vbLf & "- Location: " & point.Location.ToString())
+      write(vbLf & "- - Northing: " & point.Northing.ToString())
+      write(vbLf & "- - Easting: " & point.Easting.ToString())
+      write(vbLf & "- - Elevation: " & point.Elevation.ToString())
+      write(vbLf & "- Description: " & _
+            Convert.ToString(point.FullDescription))
+      write(vbLf & "- Raw Description: " & _
+            Convert.ToString(point.RawDescription))
+    End Sub
+  End Class
+End Namespace
