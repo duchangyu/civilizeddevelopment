ec32180947665b901241198059a4755c5eb88c5b
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2012-07-27 11:27:35 -0400

    Added class to demonstrate labels for alignments and sample drawing to use for the demo.

---
+++ dev/src/samples/NetSamples/Samples/Alignments/AlignmentLabelCommands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil.DatabaseServices;
+using Autodesk.Civil.DatabaseServices.Styles;
+
+[assembly: CommandClass(
+    typeof(Autodesk.CivilizedDevelopment.AlignmentLabelCommands))]
+
+namespace Autodesk.CivilizedDevelopment
+{
+    public class AlignmentLabelCommands : SimpleDrawingCommand
+    {
+        
+    }
+}
