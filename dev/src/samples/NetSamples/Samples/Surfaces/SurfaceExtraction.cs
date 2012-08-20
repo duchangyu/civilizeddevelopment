e6499fedbe02ad93f1975e9112284f350ffce125
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2012-08-20 14:58:30 -0400

    Adding sample to extract grid from a TIN Surface.

---
+++ dev/src/samples/NetSamples/Samples/Surfaces/SurfaceExtraction.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.Civil;
+using Autodesk.Civil.DatabaseServices;
+
+
+[assembly: CommandClass(
+    typeof(Autodesk.CivilizedDevelopment.SurfaceExtractionCommands))]
+
+namespace Autodesk.CivilizedDevelopment
+{
+    public class SurfaceExtractionCommands : SimpleDrawingCommand
+    {
+        [CommandMethod("CDS_ExtractGrid")]
+        public void CDS_ExtractGrid()
+        {
+            ObjectId surfaceId = promptForTinSurface();
+            if (surfaceId == ObjectId.Null)
+            {
+                write("\nNo TIN Surface selected.");
+                return;
+            }
+
+            using (Transaction tr = startTransaction())
+            {
+                TinSurface surface = surfaceId.GetObject(OpenMode.ForRead)
+                    as TinSurface;
+                ObjectIdCollection ids = surface.ExtractGridded(
+                    SurfaceExtractionSettingsType.Model);
+                
+                foreach (ObjectId id in ids)
+                {
+                    Polyline3d polyline =
+                        id.GetObject(OpenMode.ForWrite) as Polyline3d;
+                    if (polyline != null)
+                    {                        
+                        using (polyline)
+                        {
+                            polyline.Color = 
+                                AutoCAD.Colors.Color.FromRgb(255, 0, 0);
+                        }
+                    }
+                }
+                tr.Commit();
+            }
+        }
+
+        private ObjectId promptForTinSurface()
+        {
+            PromptEntityOptions options = new PromptEntityOptions(
+                "\nSelect a TIN Surface: ");
+            options.SetRejectMessage(
+                "\nThe selected object is not a TIN Surface.");
+            options.AddAllowedClass(typeof(TinSurface), true);
+
+            PromptEntityResult result = _editor.GetEntity(options);
+            if (result.Status == PromptStatus.OK)
+            {
+                // Everything is cool; we return the selected
+                // surface ObjectId.
+                return result.ObjectId;
+            }
+            return ObjectId.Null;   // Indicating error.
+        }
+    }
+}
---
+++ dev/src/samples/NetSamplesVB/Samples/Surfaces/SurfaceExtraction.vb
@@ -0,0 +1,56 @@
+﻿Imports Autodesk.AutoCAD.DatabaseServices
+Imports Autodesk.AutoCAD.EditorInput
+Imports Autodesk.AutoCAD.Runtime
+Imports Autodesk.Civil
+Imports Autodesk.Civil.DatabaseServices
+
+
+<Assembly: CommandClass(
+  GetType(Autodesk.CivilizedDevelopment.SurfaceExtractionCommands))> 
+
+Namespace Autodesk.CivilizedDevelopment
+  Public Class SurfaceExtractionCommands
+    Inherits SimpleDrawingCommand
+    <CommandMethod("CDS_ExtractGrid")> _
+    Public Sub CDS_ExtractGrid()
+      Dim surfaceId As ObjectId = promptForTinSurface()
+      If surfaceId = ObjectId.Null Then
+        write(vbLf & "No TIN Surface selected.")
+        Return
+      End If
+
+      Using tr As Transaction = startTransaction()
+        Dim surface As TinSurface =
+          TryCast(surfaceId.GetObject(OpenMode.ForRead), TinSurface)
+        Dim ids As ObjectIdCollection = surface.ExtractGridded(
+          SurfaceExtractionSettingsType.Model)
+        For Each id As ObjectId In ids
+          Dim polyline As Polyline3d =
+            TryCast(id.GetObject(OpenMode.ForWrite), Polyline3d)
+          If polyline IsNot Nothing Then
+            Using polyline
+              polyline.Color = AutoCAD.Colors.Color.FromRgb(255, 0, 0)
+            End Using
+          End If
+        Next
+        tr.Commit()
+      End Using
+    End Sub
+
+    Private Function promptForTinSurface() As ObjectId
+      Dim options As New PromptEntityOptions(vbLf & "Select a TIN Surface: ")
+      options.SetRejectMessage(vbLf & _
+                               "The selected object is not a TIN Surface.")
+      options.AddAllowedClass(GetType(TinSurface), True)
+
+      Dim result As PromptEntityResult = _editor.GetEntity(options)
+      If result.Status = PromptStatus.OK Then
+        ' Everything is cool; we return the selected
+        ' surface ObjectId.
+        Return result.ObjectId
+      End If
+      Return ObjectId.Null
+      ' Indicating error.
+    End Function
+  End Class
+End Namespace
