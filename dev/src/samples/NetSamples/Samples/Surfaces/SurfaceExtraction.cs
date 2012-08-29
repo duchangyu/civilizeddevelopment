b43ee47bddaac1dc9ad0884977853d753eff9ca5
Isaac Rodriguez <isaac.rodriguez@autodesk.com>
2012-08-29 09:44:09 -0400

    Implemented command CDS_FindClosestPointOnSurface
    The command prompts the user for a surface and a point outside of the surface and finds the closest point in the surface border to the specified point.

--- dev/src/samples/NetSamples/Samples/Surfaces/SurfaceExtraction.cs
+++ dev/src/samples/NetSamples/Samples/Surfaces/SurfaceExtraction.cs
@@ -2,6 +2,7 @@
 
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.Civil;
 using Autodesk.Civil.DatabaseServices;
@@ -44,6 +45,55 @@ namespace Autodesk.CivilizedDevelopment
                         }
                     }
                 }
+                tr.Commit();
+            }
+        }
+
+        [CommandMethod("CDS_FindClosestPointOnSurface")]
+        public void CDS_FindClosestPointOnSurface()
+        {
+            ObjectId surfaceId = promptForTinSurface();
+            if (surfaceId.IsNull)
+            {
+                write("\nNo TIN Surface selected.");
+                return;
+            }
+
+            PromptPointResult result = _editor.GetPoint("\nSelect point outside surface: ");
+            if (result.Status != PromptStatus.OK)
+            {
+                write("\nNo point selected.");
+                return;
+            }
+
+            Point3d selectedPoint = result.Value;
+            Point3d closestPointFound = Point3d.Origin;
+            double shortestDistanceSoFar = Double.MaxValue;
+
+            using (Transaction tr = startTransaction())
+            {
+                TinSurface surface = surfaceId.GetObject(OpenMode.ForRead) as TinSurface;
+                ObjectIdCollection borders = surface.ExtractBorder(SurfaceExtractionSettingsType.Model);
+                foreach (ObjectId borderId in borders)
+                {
+                    
+                    Polyline3d border = borderId.GetObject(OpenMode.ForRead) as Polyline3d;
+                    Point3d closestToBorder = border.GetClosestPointTo(selectedPoint, false);
+                    double distance = selectedPoint.DistanceTo(closestToBorder);
+                    if (distance < shortestDistanceSoFar)
+                    {
+                        closestPointFound = closestToBorder;
+                        shortestDistanceSoFar = distance;
+                    }
+                }
+            }
+
+            using (Transaction tr = startTransaction())
+            {
+                BlockTableRecord btr = tr.GetObject(_database.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+                Line line = new Line(selectedPoint, closestPointFound);
+                btr.AppendEntity(line);
+                tr.AddNewlyCreatedDBObject(line, true);
                 tr.Commit();
             }
         }
